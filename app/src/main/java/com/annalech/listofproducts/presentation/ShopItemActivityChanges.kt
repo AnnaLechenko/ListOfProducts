20d1caf559790d4601b460c7258d7f8bd01e5b87
Anna <anya.lechenko@mail.ru>
2024-11-25 19:41:04 +0300

    add ViewMpdel layer two

---
+++ app/src/main/java/com/annalech/listofproducts/presentation/ShopItemActivityChanges.kt
@@ -0,0 +1,16 @@
+package com.annalech.listofproducts.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.annalech.listofproducts.R
+
+class ShopItemActivityChanges : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item_changes)
+
+    }
+}
