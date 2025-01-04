b96e451ff750470b98051d7e98bb4fea46b9bb04
Anna <anya.lechenko@mail.ru>
2025-01-04 14:46:54 +0300

    update version kotlin and add in gradle depend

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -32,6 +33,7 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+
     }
 }
 
@@ -45,4 +47,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.viewModel)
+    implementation(libs.room)
+    ksp(libs.room.compiler)
+    implementation(libs.room.ktx)
 }
