e3915dafa2be69cda6ce5ba20cade65088b2fe87
Anna <anya.lechenko@mail.ru>
2025-01-19 16:09:14 +0300

    this work

--- app/src/main/java/com/annalech/listofproducts/presentation/ShopItemFragment.kt
+++ app/src/main/java/com/annalech/listofproducts/presentation/ShopItemFragment.kt
@@ -74,6 +74,7 @@ class ShopItemFragment
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
